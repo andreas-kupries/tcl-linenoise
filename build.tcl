7f8171fc06758433ebe9f7065cb2e0b69af92e13
Andreas Kupries <andreask@activestate.com>
2013-10-18 15:47:37 -0700

    Build tweak, kettle references added.

--- build.tcl
+++ build.tcl
@@ -1,6 +1,8 @@
 #!/bin/sh
 # -*- tcl -*- \
-# https://chiselapp.com/user/andreas_kupries/repository/Kettle \
 exec kettle -f "$0" "${1+$@}"
+# For kettle sources, documentation, etc. see
+# - http://core.tcl.tk/akupries/kettle
+# - http://chiselapp.com/user/andreas_kupries/repository/Kettle
 kettle critcl3
 kettle gh-pages
